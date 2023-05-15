2a3cab657b8ffbc64927e5488175bdcd766dd7a6
Patrick2402 <patryk.zawieja24@gmail.com>
2023-05-15 18:36:51 +0200

    adjustments

--- m.py
+++ m.py
@@ -1,9 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def rysuj_wykres_sin(amplituda, czestotliwosc, fazowka):
+def display_sin(amplituda, czestotliwosc, phi):
     czas = np.linspace(0, 2*np.pi, 1000)  # Przedział czasu od 0 do 2π
-    wartosci = amplituda * np.sin(czestotliwosc * czas + fazowka)  # Obliczanie wartości sinusa
+    wartosci = amplituda * np.sin(czestotliwosc * czas + phi)  # Obliczanie wartości sinusa
 
     # Rysowanie wykresu
     plt.plot(czas, wartosci)
@@ -16,5 +16,5 @@ def rysuj_wykres_sin(amplituda, czestotliwosc, fazowka):
 # Przykładowe wywołanie funkcji
 amplituda = 1.5
 czestotliwosc = 2
-fazowka = np.pi/2
-rysuj_wykres_sin(amplituda, czestotliwosc, fazowka)
+phi = np.pi/2
+display_sin(amplituda, czestotliwosc, fazowka)
