e9aa32909234d761bce55e2378f6cc28643c3586
Patrick2402 <patryk.zawieja24@gmail.com>
2023-05-15 18:41:57 +0200

    jakies triki na trzepaku byle zdać 

--- m.py
+++ m.py
@@ -1,20 +1,53 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def display_sin(amplituda, czestotliwosc, phi):
+def rysuj_sumowane_sinusy(amplituda1, czestotliwosc1, fazowka1, amplituda2, czestotliwosc2, fazowka2):
     czas = np.linspace(0, 2*np.pi, 1000)  # Przedział czasu od 0 do 2π
-    wartosci = amplituda * np.sin(czestotliwosc * czas + phi)  # Obliczanie wartości sinusa
+    sinus1 = amplituda1 * np.sin(czestotliwosc1 * czas + fazowka1)  # Obliczanie wartości pierwszego sinusa
+    sinus2 = amplituda2 * np.sin(czestotliwosc2 * czas + fazowka2)  # Obliczanie wartości drugiego sinusa
+    suma = sinus1 + sinus2  # Sumowanie sinusoid
 
-    # Rysowanie wykresu
-    plt.plot(czas, wartosci)
+    # Rysowanie wykresu sumy sinusoid
+    plt.subplot(3, 1, 1)
+    plt.plot(czas, sinus1, label='Sinus 1')
+    plt.plot(czas, sinus2, label='Sinus 2')
+    plt.plot(czas, suma, label='Suma')
     plt.xlabel('Czas')
     plt.ylabel('Wartość sinusa')
-    plt.title('Wykres sinusa')
+    plt.title('Sumowanie dwóch sinusoid')
     plt.grid(True)
+    plt.legend()
+
+    # Obliczanie transformaty Fouriera
+    transformata = np.fft.fft(suma)
+    czestotliwosci = np.fft.fftfreq(len(suma), d=1/(2*np.pi))
+
+    # Rysowanie wykresu transformaty Fouriera
+    plt.subplot(3, 1, 2)
+    plt.plot(czestotliwosci, np.abs(transformata))
+    plt.xlabel('Częstotliwość')
+    plt.ylabel('Amplituda')
+    plt.title('Transformaty Fouriera')
+    plt.grid(True)
+
+    # Rysowanie wykresu fazowego
+    plt.subplot(3, 1, 3)
+    plt.plot(czas, np.angle(transformata))
+    plt.xlabel('Czas')
+    plt.ylabel('Faza')
+    plt.title('Wykres fazowy')
+    plt.grid(True)
+
+    plt.tight_layout()
     plt.show()
 
 # Przykładowe wywołanie funkcji
-amplituda = 1.5
-czestotliwosc = 2
-phi = np.pi/2
-display_sin(amplituda, czestotliwosc, fazowka)
+amplituda1 = 1.5
+czestotliwosc1 = 2
+fazowka1 = np.pi/2
+
+amplituda2 = 0.8
+czestotliwosc2 = 3
+fazowka2 = np.pi
+
+rysuj_sumowane_sinusy(amplituda1, czestotliwosc1, fazowka1, amplituda2, czestotliwosc2, fazowka2)
